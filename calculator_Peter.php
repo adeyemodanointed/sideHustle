5549bb3c0e36584dc73aa93e6853f49d5174ebd0
Peter Adeyemo <adeyemodanointed@yahoo.com>
2021-02-22 23:34:38 +0100

    modified

--- calculator_Peter.php
+++ calculator_Peter.php
@@ -1,13 +1,29 @@
 <?php
+    for($i=0; $i < 4; $i++){
+        $calc = $i;
+        $x = 10;
+        $y = 2;
+        
+        switch($calc){
+            case 0:
+                echo "Addition gives: ";
+                echo ( $x * $y . "<br>");
+                break;
 
-    $calculate = 1;
+            case 1:
+                echo "Subtraction gives: ";
+                echo ($x - $y . "<br>");
+                break;
 
-    switch($calculate) {
-        case 1:
-            echo "This is for addition, input two numbers";
-            $a = rtrim(fgets(STDIN));
-            $b = rtrim(fgets(STDIN));
-            echo("Sum of numbers is: $a + $b");
+            case 2:
+                echo "Division gives: ";
+                echo ($x / $y . "<br>");
+                break;
 
+            case 3:
+                echo "Multiplication gives: ";
+                echo ($x * $y);
+                break;
+        }
     }
 ?>
